dd410d4f9490846463d18ea560473243530d7b53
Sabrina Couto <sabrinacoutolima@gmail.com>
2021-11-24 14:39:33 -0300

    Novas páginas adicionadas e fases do Shrek conluídas

---
+++ Shrek/Fase 1/index.js
@@ -0,0 +1,13 @@
+function escolhaShrek () {
+  var escolha = prompt ('O que ele deve fazer? Escolha entre a opção 1 e 2');
+  if (escolha == 1) {
+      location = "./shrekover.html";
+  } else if (escolha == 2) {
+ location = "../Fase 2/shrek2.html" }
+else {
+  alert("Opa, algo deu errado. Não se esqueça de escolher entre os números 1 e 2");
+}
+}
+
+
+
---
+++ Shrek/Fase 2/index.js
@@ -0,0 +1,10 @@
+function escolhaShrek2() {
+    var escolha = prompt ('O que ele deve fazer? Escolha entre a opção 1 e 2');
+    if (escolha == 1) {
+        location = "../Fase 3/shrek3.html";
+    } else if (escolha == 2) {
+   location = "./shrekover2.html" }
+  else {
+    alert("Opa, algo deu errado. Não se esqueça de escolher entre os números 1 e 2");
+  }
+  }
