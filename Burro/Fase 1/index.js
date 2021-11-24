f3d6a70d5948f53fdd9c75dc8296774ba5dc9d74
Sabrina Couto <sabrinacoutolima@gmail.com>
2021-11-24 19:01:41 -0300

    Finalização do projeto

---
+++ Burro/Fase 1/index.js
@@ -0,0 +1,10 @@
+function escolhaBurro () {
+    var escolha = prompt ('O que ele deve fazer? Escolha entre a opção 1 e 2');
+    if (escolha == 1) {
+        location = "./burro-over.html";
+    } else if (escolha == 2) {
+   location = "../Fase 2/burro2.html" }
+  else {
+    alert("Opa, algo deu errado. Não se esqueça de escolher entre os números 1 e 2");
+  }
+  }
---
+++ Burro/Fase 2/index.js
@@ -0,0 +1,10 @@
+function escolhaBurro2 () {
+    var escolha = prompt ('O que ele deve fazer? Escolha entre a opção 1 e 2');
+    if (escolha == 1) {
+        location = "./burro-over2.html";
+    } else if (escolha == 2) {
+   location = "../Fase 3/burro3.html" }
+  else {
+    alert("Opa, algo deu errado. Não se esqueça de escolher entre os números 1 e 2");
+  }
+  }
