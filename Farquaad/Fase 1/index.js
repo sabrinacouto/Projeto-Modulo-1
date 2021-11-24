71bb6c92b55a1295a12bccd2629446e2e1dd927c
Sabrina Couto <sabrinacoutolima@gmail.com>
2021-11-24 16:34:51 -0300

    Novas fases do jogo adicionadas e fases do Farquaad finalizadas

---
+++ Farquaad/Fase 1/index.js
@@ -0,0 +1,10 @@
+function escolhaFarquaad () {
+    var escolha = prompt ('O que ele deve fazer? Escolha entre a opção 1 e 2');
+    if (escolha == 1) {
+        location = "../Fase 2/farquaad2.html";
+    } else if (escolha == 2) {
+   location = "./farquaadover.html" }
+  else {
+    alert("Opa, algo deu errado. Não se esqueça de escolher entre os números 1 e 2");
+  }
+  }
---
+++ Farquaad/Fase 3/index.js
@@ -0,0 +1,10 @@
+function escolhaFarquaad3 () {
+    var escolha = prompt ('O que ele deve fazer? Escolha entre a opção 1 e 2');
+    if (escolha == 1) {
+        location = "./farquaadfinal.html";
+    } else if (escolha == 2) {
+   location = "./farquaadover3.html" }
+  else {
+    alert("Opa, algo deu errado. Não se esqueça de escolher entre os números 1 e 2");
+  }
+  }
